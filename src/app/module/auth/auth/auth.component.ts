831648cd736758dee1d76f167511ca525db20816
dev-waqar <waansari2001@gmail.com>
2026-04-01 21:34:13 +0530

    initial commit

---
+++ src/app/module/auth/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from "../signup/signup.component";
+
+@Component({
+  selector: 'app-auth',
+  imports: [LoginComponent, SignupComponent],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+}
