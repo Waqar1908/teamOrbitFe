e45a49de2a53f3d0b056e6481cb15e3dd2ab35b5
dev-waqar <waansari2001@gmail.com>
2026-04-11 16:03:20 +0530

    waqar ansari: dashboard fundamentsal with taiwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,45 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-{
-    path:'',
-    loadComponent:()=> import('./module/auth/auth/auth.component').then(m=>m.AuthComponent)
-}
+    {
+        path: '',
+        loadComponent: () => import('./module/auth/auth/auth.component').then(m => m.AuthComponent)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./module/dashboard/main/main.component').then(m => m.MainComponent),
+        children: [
+           
+            {
+                path:'',
+                loadComponent: () => import('./module/dashboard/dashboard-main/dashboard-main.component').then(m => m.DashboardMainComponent),
+                pathMatch:'full'
+            },
+             {
+                path:'employees',
+                loadComponent: () => import('./module/dashboard/employee/employee.component').then(m => m.EmployeeComponent),
+            },
+            {
+                path:'attendance',
+                loadComponent: () => import('./module/dashboard/attendance/attendance.component').then(m => m.AttendanceComponent),
+            },
+            {
+                path:'company',
+                loadComponent: () => import('./module/dashboard/company/company.component').then(m => m.CompanyComponent),
+            },
+            {   
+                path:'holidays',    
+                loadComponent:()=>import('./module/dashboard/holiday/holiday.component').then(m=>m.HolidayComponent)
+            },
+            {
+                path:'reports',
+                loadComponent:()=>import('./module/dashboard/analysis/analysis.component').then(m=>m.AnalysisComponent)
+            },
+            {
+                path:'settings',
+                loadComponent:()=>import('./module/dashboard/setting/setting.component').then(m=>m.SettingComponent)
+
+            }
+        ]
+    }
 ];
---
+++ src/app/module/dashboard/side-menu/side-menu.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [CommonModule],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss'
+})
+export class SideMenuComponent {
+  isCollapsed = false;
+  menuItems = [
+  {
+    label: 'Dashboard',
+    icon: 'pi pi-home',
+    route: '/dashboard',
+    exact: true
+  },
+  {
+    label: 'Employee',
+    icon: 'pi pi-users',
+    route: '/dashboard/employees'
+  },
+  {
+    label: 'Attendance',
+    icon: 'pi pi-clock',
+    route: '/dashboard/attendance'
+  },
+  {
+    label: 'Company Detail',
+    icon: 'pi pi-building',
+    route: '/dashboard/company'
+  },
+  {
+    label: 'Holiday',
+    icon: 'pi pi-calendar',
+    route: '/dashboard/holidays'
+  },
+  {
+    label: 'Analysis',
+    icon: 'pi pi-chart-line',
+    route: '/dashboard/reports'
+  },
+  {
+    label: 'Setting',
+    icon: 'pi pi-cog',
+    route: '/dashboard/settings'
+  }
+];
+
+constructor( private router: Router) {}
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  navigate(item: any) {
+    console.log('Navigating to:', item.route);
+    this.router.navigate([item.route]); 
+  }
+
+}
